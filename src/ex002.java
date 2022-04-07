209a1e8293af986593e6942f26c16a034272642c
danielcsmg <daniel.csouzamg@gmail.com>
2022-04-07 00:48:34 -0300

    Mais conteúdo

--- src/ex002.java
+++ src/ex002.java
@@ -13,9 +13,16 @@ public class ex002 {
         int menor = menorValor(vetorA);
 
         System.out.println("Se é o menor valor que vc quer, entao recebaaaa "+menor);
+
+        int[] vetorOrganizado = organizaValores(vetorA);
+
+        System.out.println("Seu vetor está organizado.");
+        for(int i = 0; i < vetorOrganizado.length; i++){
+            System.out.println("Na posição " + i + ": " + vetorOrganizado[i]);
+        }
     }
 
-    
+
     public static int menorValor(int[] vetorP){
         int numMenor = vetorP[0];
         for(int i = 0 ; i < vetorP.length; i++){
@@ -25,5 +32,19 @@ public class ex002 {
             }
         }
         return numMenor;
+    }
+
+    public static int[] organizaValores(int[] vetor){
+        int trocador = 0;
+        for(int i = 0; i < vetor.length; i++){
+            for(int j = i; j < vetor.length;j++){
+                if(vetor[i] > vetor[j]){
+                    trocador = vetor[j];
+                    vetor[j] = vetor[i];
+                    vetor[i] = trocador;
+                }
+            }
+        }
+        return vetor;
     }
 }
